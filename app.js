8a0d568d3127592549821769d2389d621cf55d8e
Yueming Fang <zergdo@gmail.com>
2021-04-10 16:55:08 +0800

    feat: update tencentcloud-sdk-nodejs

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
 
+const tencentCloudSdk = require('tencentcloud-sdk-nodejs');
 const Nlp = require('./lib/nlp');
 
 const createNlp = options => {
   const nlp = new Nlp({ ...options });
-  const { tencentCloud } = options;
+  const tencentCloud = options.tencentCloud || tencentCloudSdk;
   nlp.tencentCloud = tencentCloud;
   return nlp;
 };
